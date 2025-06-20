6f94ab4944893025e46cce8543cf43d35c95c584
FR4TLIZ <kac13s13pow@gmail.com>
2025-06-20 13:49:49 +0200

    nowe

--- src/main/java/servlets/module/challenge/SqlInjection1.java
+++ src/main/java/servlets/module/challenge/SqlInjection1.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.servlet.ServletException;
